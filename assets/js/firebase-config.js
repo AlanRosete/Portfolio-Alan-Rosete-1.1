60b8ea05da4bb127548a79c1a014119fe262eab2
AlanRosete1 <alanroset3@gmail.com>
2025-01-19 14:29:50 -0600

    update firebase logs

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -3,16 +3,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore, addDoc, collection } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBYZbpJ0DNi9vzWfifECduUmdfhfoAhMZ4",
-  authDomain: "form-main-portfolio.firebaseapp.com",
-  projectId: "form-main-portfolio",
-  storageBucket: "form-main-portfolio.appspot.com",
-  messagingSenderId: "908324247776",
-  appId: "1:908324247776:web:6544040ed668d602a81885",
-  measurementId: "G-SQB9XBFPPS"
+  apiKey: "AIzaSyAXEIw5w6G1HxexuATgXVHBPd5kNJ27dq8",
+  authDomain: "test-form-28517.firebaseapp.com",
+  projectId: "test-form-28517",
+  storageBucket: "test-form-28517.firebasestorage.app",
+  messagingSenderId: "770454833881",
+  appId: "1:770454833881:web:f295ae765f7e0083fd4cc8",
+  measurementId: "G-7MSNYDEJWT"
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
