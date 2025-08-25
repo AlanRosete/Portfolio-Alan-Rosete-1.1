7e50ca4932ecdf9eb7049762b88950af0549a770
Alan Rosete <78501259+AlanRosete@users.noreply.github.com>
2025-08-25 00:59:35 -0600

    Update Firebase Keysss

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,23 +1,20 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, addDoc, collection } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqV_Vs_SdttCNMbxOilkwRgRGJexY0joY",
-  authDomain: "form-portfolio-db.firebaseapp.com",
-  projectId: "form-portfolio-db",
-  storageBucket: "form-portfolio-db.firebasestorage.app",
-  messagingSenderId: "114797988180",
-  appId: "1:114797988180:web:e6c2888752527ce6fae0c1",
-  measurementId: "G-E6HX02DB8M"
+  apiKey: "AIzaSyBhS_HiO1d6SnW32fyytvwXiu6nah3x4Hg",
+  authDomain: "add-form-portfolio.firebaseapp.com",
+  projectId: "add-form-portfolio",
+  storageBucket: "add-form-portfolio.firebasestorage.app",
+  messagingSenderId: "809001225924",
+  appId: "1:809001225924:web:71ff4c459dc16348efc391"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
-// Function Send Data 
 export async function sendFormData(name, email, message) {
   try {
     await addDoc(collection(db, "contacts"), {
@@ -25,9 +22,9 @@ export async function sendFormData(name, email, message) {
       email,
       message,
     });
-    return true; // Good 
+    return true;
   } catch (error) {
     console.error("Error: ", error);
-    return false; // Internal Error
+    return false;
   }
 }
