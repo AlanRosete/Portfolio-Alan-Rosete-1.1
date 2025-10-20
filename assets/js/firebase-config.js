82e4d6c23c01238c683cebc01b72bb086ba9db33
Alan Rosete <78501259+AlanRosete@users.noreply.github.com>
2025-10-19 23:42:56 -0600

    Aplicando ENV DB FIREBASE OK

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,14 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, addDoc, collection } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, addDoc, collection } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
+// import 'dotenv/config';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhS_HiO1d6SnW32fyytvwXiu6nah3x4Hg",
-  authDomain: "add-form-portfolio.firebaseapp.com",
-  projectId: "add-form-portfolio",
-  storageBucket: "add-form-portfolio.firebasestorage.app",
-  messagingSenderId: "809001225924",
-  appId: "1:809001225924:web:71ff4c459dc16348efc391"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
