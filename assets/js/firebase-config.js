9fc3201e7c80b87a39b9b2342a65cb27720ccf3a
Alan Rosete <78501259+AlanRosete@users.noreply.github.com>
2026-02-18 02:24:28 -0600

    Parcel Try Env PROD

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,13 +1,28 @@
 const env = (typeof window !== "undefined" && window.env) || {};
 
+let parcelEnv = {};
+try {
+  parcelEnv = {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+  };
+} catch (e) {
+  console.log("Error en variables parcel", e);
+}
+
 export const firebaseConfig = {
-  apiKey: process.env.API_KEY || env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN || env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID || env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET || env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID || env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID || env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID || env.MEASUREMENT_ID,
+  apiKey: parcelEnv.API_KEY || env.API_KEY,
+  authDomain: parcelEnv.AUTH_DOMAIN || env.AUTH_DOMAIN,
+  projectId: parcelEnv.PROJECT_ID || env.PROJECT_ID,
+  storageBucket: parcelEnv.STORAGE_BUCKET || env.STORAGE_BUCKET,
+  messagingSenderId: parcelEnv.MESSAGING_SENDER_ID || env.MESSAGING_SENDER_ID,
+  appId: parcelEnv.APP_ID || env.APP_ID,
+  measurementId: parcelEnv.MEASUREMENT_ID || env.MEASUREMENT_ID,
 };
 
 console.log("Firebase config loaded:", firebaseConfig);
